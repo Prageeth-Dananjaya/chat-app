a35981957da0c942ae9f7af2a45a9e17a02f58c6
prageeth dananjaya <e19431@eng.pdn.ac.lk>
2025-09-21 16:47:18 +0530

    signup controller added

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,56 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/util.js";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" }); // 400  --> Bad request
+    }
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 8 characters" });
+    }
+    // check if emails valid: regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const user = await User.findOne({ email: email });
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    // Hashing the password
+    const salt = await bcrypt.genSalt(10); // 10 defines the length of the hashed string
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+
+      // todo: send a welcome email to the user
+      
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
